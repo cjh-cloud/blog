a3c030143941aab7c506017d4fd121b9ccb678c3
SenorGrande <clooneyjh@gmail.com>
2022-02-12 18:36:33 +1300

    feat: infra

---
+++ infra/README.md
@@ -0,0 +1,10 @@
+
+Log into Azure:
+`az login`
+
+Set up AWS creds:
+`$env:AWS_ACCESS_KEY_ID = ""`
+`$env:AWS_SECRET_ACCESS_KEY = ""`
+
+Run Pulumi:
+`pulumi up`
--- infra/index.ts
+++ infra/index.ts
@@ -1,6 +1,7 @@
 import * as pulumi from "@pulumi/pulumi";
 import * as resources from "@pulumi/azure-native/resources";
 import * as azure_native from "@pulumi/azure-native";
+import * as aws from "@pulumi/aws";
 
 const config = new pulumi.Config();
 
@@ -24,6 +25,39 @@ const staticSiteCustomDomain = new azure_native.web.StaticSiteCustomDomain("stat
     domainName: "cjscloud.city",
     name: staticSite.name,
     resourceGroupName: resourceGroup.name,
+    validationMethod: 'dns-txt-token'
 });
 
-export const url = staticSite.contentDistributionEndpoint;
+// AWS S3 bucket for redirection
+// Create an AWS resource (S3 Bucket)
+const bucket = new aws.s3.Bucket("cjscloud.city", {
+    bucket: "cjscloud.city",
+    website: {
+        redirectAllRequestsTo: "www.cjscloud.city"
+    },
+});
+
+// todo : add records to hostzone route53
+const selected = aws.route53.getZone({
+    name: "cjscloud.city.",
+    privateZone: false,
+});
+const www = new aws.route53.Record("www", {
+    zoneId: selected.then((selected: { zoneId: any; }) => selected.zoneId),
+    name: selected.then((selected: { name: any; }) => `www.${selected.name}`),
+    type: "CNAME",
+    ttl: 300,
+    records: [staticSite.defaultHostname],
+});
+const apex = new aws.route53.Record("apex", {
+    zoneId: selected.then((selected: { zoneId: any; }) => selected.zoneId),
+    name: selected.then((selected: { name: any; }) => `${selected.name}`),
+    type: "A",
+    aliases: [{
+        name: "s3-website-ap-southeast-2.amazonaws.com",
+        zoneId: "Z1WCIGYICN2BYD", // https://docs.aws.amazon.com/general/latest/gr/s3.html#s3_website_region_endpoints
+        evaluateTargetHealth: false,
+    }],
+});
+
+export const url = staticSite.customDomains;
