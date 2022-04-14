6ec02db9ccac11b7ef0103e3add0210ac4edddb2
SenorGrande <clooneyjh@gmail.com>
2022-04-14 20:01:51 +1200

    feat: test using gtag in head of html setting head to true does this commenting out rest for now, also getting an error where the origin is used twice

--- gatsby-config.js
+++ gatsby-config.js
@@ -58,13 +58,13 @@ module.exports = {
         // This object is used for configuration specific to this plugin
         pluginConfig: {
           // Puts tracking script in the head instead of the body
-          head: false,
-          // Setting this parameter is also optional
-          respectDNT: true,
-          // Avoids sending pageview hits from custom paths
-          exclude: ["/preview/**", "/do-not-track/me/too/"],
-          // Defaults to https://www.googletagmanager.com
-          origin: "www.cjscloud.city",
+          head: true,
+          // // Setting this parameter is also optional
+          // respectDNT: true,
+          // // Avoids sending pageview hits from custom paths
+          // exclude: ["/preview/**", "/do-not-track/me/too/"],
+          // // Defaults to https://www.googletagmanager.com
+          // origin: "www.cjscloud.city",
         },
       },
     }
